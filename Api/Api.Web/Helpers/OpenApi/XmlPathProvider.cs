bd14424c19f1415ff152eeb4ea16064073124cb3
Alexander <50077233+Sekir0@users.noreply.github.com>
2021-01-28 16:12:13 +0200

    configured Swagger

---
+++ Api/Api.Web/Helpers/OpenApi/XmlPathProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Api.Web.Helpers.OpenApi
+{
+    public static class XmlPathProvider
+    {
+        public static string XmlPath => Path.Combine(
+            AppContext.BaseDirectory,
+            $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
+    }
+}
--- Api/Api.Web/Startup.cs
+++ Api/Api.Web/Startup.cs
@@ -1,3 +1,4 @@
+using Api.Web.Helpers.OpenApi;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,10 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace Api.Web
 {
@@ -25,13 +23,32 @@ namespace Api.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Reactions",
+                    Version = "1.0.0"
+                });
+
+                options.OperationFilter<OperationIdFilter>();
+
+                options.IncludeXmlComments(XmlPathProvider.XmlPath);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app
+                    .UseSwagger()
+                    .UseSwaggerUI(config => { config.SwaggerEndpoint("/swagger/v1/swagger.json", "Profiles Api V1"); });
+
+                app.UseCors(builder => builder.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin());
             }
 
             app.UseHttpsRedirection();
